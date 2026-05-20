77a381f1cedb13d5c2925eab3f1a455c7155993f
Zamorano <zamoranofragoso@gmail.com>
2026-05-19 21:41:47 -0300

    POO básico

---
+++ POO-Básico/Exercicio01/Program.cs
@@ -0,0 +1,32 @@
+﻿//Criar uma classe que representa uma conta bancária, com um número indicador, titular, saldo e senha
+//Criar um objeto do tipo Conta, adicionar dados e mostrar as informações titular e saldo no console, utilizando interpolação de strings.
+//Desenvolver um método da classe Conta que exibe suas informações.
+
+ContaBancaria conta1 = new ContaBancaria();
+conta1.id = 1234;
+conta1.nomeTitular = "João da Silva";
+conta1.saldoConta = 123.24;
+conta1.senha = "minhasenha123";
+conta1.exibirInformacoes();
+
+ContaBancaria conta2 = new ContaBancaria();
+conta2.id = 5678;
+conta2.nomeTitular = "Pedro da Silva";
+conta2.saldoConta = 1230.44;
+conta2.senha = "121212";
+conta2.exibirInformacoes();
+
+class ContaBancaria
+{
+    public int id;
+    public string nomeTitular;
+    public double saldoConta;
+    public string senha;
+
+    public void exibirInformacoes()
+    {
+        Console.WriteLine($"\n\nIdentificador da conta: {id}");
+        Console.WriteLine($"Nome do titular: {nomeTitular}");
+        Console.WriteLine($"Saldo atual da conta: {saldoConta:F2}");
+    }
+}
---
+++ POO-Básico/Exercicio02/Program.cs
@@ -0,0 +1,28 @@
+﻿//Desenvolver uma classe que modele um carro, e que contenha os métodos acelerar, frear e buzinar
+Carro uno = new Carro();
+uno.acelerar();
+uno.frear();
+uno.buzinar();
+
+Carro celta = new Carro();
+celta.acelerar();
+celta.frear();
+celta.buzinar();
+
+class Carro
+{
+    public void acelerar()
+    {
+        Console.WriteLine("Acelerando");
+    }
+
+    public void frear()
+    {
+        Console.WriteLine("Freando");
+    }
+
+    public void buzinar()
+    {
+        Console.WriteLine("Buzinando");
+    }
+}
